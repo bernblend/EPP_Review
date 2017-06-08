de8d04a93d56d20fc34a238d9aa671483e462a3f
bernblend <bernblend@gmail.com>
2017-06-07 21:55:58 -0700

    Created travelCost.cpp

---
+++ travelCost.cpp
@@ -0,0 +1,15 @@
+/*
+  travelcost.cpp
+  June 7, 2017
+  bernblend
+*/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+  //variables
+  double mpg, distance, gallons, pricePerGallon, totalCost;
+  //output using insertion operator
+  return 0;
+}
