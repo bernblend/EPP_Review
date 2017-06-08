0e3f6b8b00a1c02d5ef0355da69432ea614ba415
bernblend <bernblend@gmail.com>
2017-06-07 22:08:55 -0700

    Added I/O

--- travelCost.cpp
+++ travelCost.cpp
@@ -3,13 +3,19 @@
   June 7, 2017
   bernblend
 */
+//directives
 #include <iostream>
 #include <iomanip>
+//
 using namespace std;
 int main()
 {
   //variables
   double mpg, distance, gallons, pricePerGallon, totalCost;
   //output using insertion operator
+  cout << "Enter distance in miles: "<< endl;
+  cin >> distance;
+  cout << "Enter price for one gallon of gas: " << endl;
+  cin >> pricePerGallon;
   return 0;
 }
