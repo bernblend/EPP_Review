dfb0605ff83ce5831860de511c2aad91d4e90497
bernblend <bernblend@gmail.com>
2017-06-08 22:57:58 -0700

    Added initial variable 'team'

--- nflRosters.cpp
+++ nflRosters.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-int int main(int argc, char const *argv[]) {
-  
+int main(int argc, char const *argv[])
+{
+  //variables
+  string team, position;
+
+  cout << "Enter NFL team: " << endl;
+  cin >> team;
+
+  cout << "\n" << team << endl;
+
   return 0;
 }
