0724b11e1b09a18d8a31e04b4fdefdd01e45f1f9
bernblend <bernblend@gmail.com>
2017-06-09 21:56:17 -0700

    Initial breakdown of variables.

--- dotProduct.cpp
+++ dotProduct.cpp
@@ -2,7 +2,29 @@
 #include <iomanip>
 #include <cmath>
 using namespace std;
-int int main(int argc, char const *argv[]) {
-  
+
+
+double dotProduct(int vectA, int vectB)
+{
+  double magA
+  double magB
+  double aTimesB
+  double result
+  double param
+
+  result = acos ( param * PI / 180.0 );
+}
+
+int int main(int argc, char const *argv[])
+{
+
+  //variables
+  double vectAi;
+  double vectAj;
+  double vectBi;
+  double vectBj;
+  static double angle;
+
+
   return 0;
 }
