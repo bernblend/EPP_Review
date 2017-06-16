3868f3e5bd3182ee7e34b47f3a9235fab2817486
bernblend <bernblend@gmail.com>
2017-06-15 23:23:07 -0700

    Created sbChampsSolo.cpp

---
+++ sbChampsSolo.cpp
@@ -0,0 +1,24 @@
+/*
+	sbChampsSolo.cpp
+	June 15, 2017
+	bernblend
+*/
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>  //read, write and create file.
+#include <ctime>
+using namespace std;
+
+
+string getsbChamp(int year)
+{
+
+}
+
+
+int main()
+{
+
+
+}
