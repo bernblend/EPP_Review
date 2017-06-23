b4716c171f8d8a01d3783fc821313ec9847e37a6
bernblend <bernblend@gmail.com>
2017-06-23 14:29:06 -0700

    Added <iomanip> functions.

--- Lynda_Files/Ch03/random.cpp
+++ Lynda_Files/Ch03/random.cpp
@@ -1,19 +1,22 @@
 /*
- * random.cpp
- *
- *  Created on: May 15, 2014
- *      Author: Peggy Fisher
- */
+	random.cpp
+  June 23, 2017
+	bernblend
+*/
 
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
 using namespace std;
 int main()
 {
 	srand(time(0));
-	for(int i = 0;i<10;i++)
+
+	cout << setw(15) << right << "Column 1" << setw(15) << right << "Column 2" << endl;
+
+	for(int i = 0;i < 10;i++)
 	{
-		cout<<rand()<<endl;
+		cout << setw(15) << right << rand() << setw(15) << right << rand() << endl;
 	}
 }
