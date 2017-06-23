b81f1cb58d7d1344d289594a73270de477c2945a
bernblend <bernblend@gmail.com>
2017-06-23 00:34:35 -0700

    Competed program - shipping.cpp

--- Lynda_Files/Ch03/shipping.cpp
+++ Lynda_Files/Ch03/shipping.cpp
@@ -1,18 +1,18 @@
 /*
-shipping.cpp
-May 20, 2014
-Peggy Fisher
+	shipping.cpp
+	June 22, 2017
+	bernblend
 */
 #include <iostream>
-#include <iomanip>
+#include <iomanip>  //for fixed and setprecision
 using namespace std;
 int main()
 {
 	double price, shipping;
-	
-	cout<<"enter the total price of the order: "<<endl;
-	cin>>price;
-	
+
+	cout << "Enter the total price of the order: " << endl;
+	cin >> price;
+
 	if(price > 75)
 		shipping = 0;
 	else if(price > 50)
@@ -21,7 +21,8 @@ int main()
 		shipping = 10;
 	else if(price > 0)
 		shipping = 15;
-	
-	cout<<"Total price including shipping is: "<<fixed<<setprecision(2)<<price + shipping<<endl;
-return 0;
+
+	cout << "Total price including shipping is: $" << fixed <<  setprecision(2) << price + shipping<<endl;
+
+  return 0;
 }
