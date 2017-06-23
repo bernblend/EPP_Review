c0652e94b6d7afe600dcf19f13ea5e8fb2a2e815
bernblend <bernblend@gmail.com>
2017-06-23 01:27:02 -0700

    Completed printodds.cpp

--- Lynda_Files/Ch03/printodds.cpp
+++ Lynda_Files/Ch03/printodds.cpp
@@ -1,18 +1,15 @@
 /*
- * printodds.cpp
- *
- *  Created on: May 13, 2014
- *      Author: Peggy Fisher
- */
-
+	printodds.cpp
+	June 22, 2017
+	bernblend
+*/
 #include <iostream>
 using namespace std;
 int main()
 {
-
 	for(int num = 1; num <= 100; num++)
 	{
-		if(num %2 != 0)
-			cout<<num<<endl;
+		if(num % 2 != 0)
+			cout << num << endl;
 	}
 }
