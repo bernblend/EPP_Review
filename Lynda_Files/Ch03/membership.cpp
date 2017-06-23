290fd91ed2971dc0663c97b2522e23e2919da7cd
bernblend <bernblend@gmail.com>
2017-06-23 00:54:15 -0700

    Completed membership.cpp

--- Lynda_Files/Ch03/membership.cpp
+++ Lynda_Files/Ch03/membership.cpp
@@ -1,10 +1,8 @@
 /*
- * membership.cpp
- *
- *  Created on: May 13, 2014
- *      Author: Peggy Fisher
+	membership.cpp
+	June 23, 2017
+ 	bernblend
  */
-
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -15,41 +13,41 @@ int main()
 	double totalCharges;
 
 	//declare constant variables for membership costs
-	const double ADULT = 40.0;
-	const double CHILD = 30.0;
-	const double FAMILY = 100.0;
+	const double ADULT = 40.00;
+	const double CHILD = 30.00;
+	const double FAMILY = 100.00;
 
 	//setup the output formatting
-	cout<<fixed<<setprecision(2)<<showpoint;
+	cout << fixed << setprecision(2) << showpoint;
 
 	do{
 		//Print out the menu:
-		cout<<"Please choose the type of membership"<<endl;
-		cout<<"1. Adult Membership ($40/month)"<<endl;
-		cout<<"2. Child Membership ($30/month)"<<endl;
-		cout<<"3. Family Membership ($100/month)"<<endl;
-		cout<<"4. Exit"<<endl;
-		cin>>menuChoice;
+		cout << "\nPlease choose the type of membership" << endl;
+		cout << "1. Adult Membership ($40/month)" << endl;
+		cout << "2. Child Membership ($30/month)" << endl;
+		cout << "3. Family Membership ($100/month)" << endl;
+		cout << "4. Exit" << endl;
+		cin >> menuChoice;
 
 		//validate the users menu choice using a while loop
 		while(menuChoice < 1 || menuChoice > 4)
 		{
-			cout<<"Invalid choice, please re-enter menu item"<<endl;
-			cin>>menuChoice;
+			cout << "Invalid choice, please re-enter menu item" << endl;
+			cin >> menuChoice;
 		}
 
 		if (menuChoice == 4)
 			break;		//this allows us to exit our do/while loop
 
 		//ask the user for duration in months of membership
-		cout<<"Enter the number of months:"<<endl;
-		cin>>months;
+		cout << "Enter the number of months:" << endl;
+		cin >> months;
 
 		//validate number of months
 		while(months <1 || months > 24)
 		{
-			cout<<"Memberships must be between 1 month and 2 years (24 months), please enter a valid number"<<endl;
-			cin>>months;
+			cout << "Memberships must be between 1 month and 2 years (24 months), please enter a valid number" << endl;
+			cin >> months;
 		}
 
 		switch(menuChoice)
@@ -67,9 +65,10 @@ int main()
 			break;
 		}
 
-		cout<<"Based on your choice, your total charges will be $"<<totalCharges<<endl<<endl;
+		cout << "\nBased on your choice, your total charges will be $" << totalCharges << endl;
 
 	} while(menuChoice != 4); 	//this allows the user to enter various membership options
-	cout<<"Thanks for choosing our gym"<<endl;
+	cout << "Thanks for choosing our gym!" << endl;
+
 	return 0;
 }
