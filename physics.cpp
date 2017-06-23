65784662e6dd57cd6bb16911ee2afd1287793be9
bernblend <bernblend@gmail.com>
2017-06-22 20:04:17 -0700

    Added Prodigy tribute on file.

--- physics.cpp
+++ physics.cpp
@@ -1,1 +1,62 @@
-// Teaching Jefe C++
+/*
+	survivalOfTheFittest.cpp
+	June 20, 2017
+	bernblend
+*/
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+
+int main()
+{
+
+  if (vodka() + milk() == prodigy() + otherguy()) {
+
+    stuffHappens();
+
+  }
+
+  cout <<	"	Yeah	"	<< endl;
+  cout <<	"	Sendin' this one out, to my man Killa be	"	<< endl;
+  cout <<	"	No doubt indeed, without weed, know what I'm sayin'?	"	<< endl;
+  cout <<	"	That old real shi'	"	<< endl;
+  cout <<	"		"	<< endl;
+  cout <<	"	There's a war goin' on outside no man is safe from	"	<< endl;
+  cout <<	"	You could run but you can't hide forever	"	<< endl;
+  cout <<	"	From these streets that we done took	"	<< endl;
+  cout <<	"	You walkin' witcha head down scared to look	"	<< endl;
+  cout <<	"	You shook cause ain't no such things as halfway crooks	"	<< endl;
+  cout <<	"	They never around when the beef cooks in my part of town	"	<< endl;
+  cout <<	"	It's similar to Vietnam	"	<< endl;
+  cout <<	"	Now we all grown up and old, and beyond the cop's control	"	<< endl;
+  cout <<	"	They better have the riot gear ready	"	<< endl;
+  cout <<	"	Tryin' to bag me and get rocked steady	"	<< endl;
+  cout <<	"	By the mac one-double, I touch you	"	<< endl;
+  cout <<	"	And leave you with not much to go home wit'	"	<< endl;
+  cout <<	"	My skin is thick, cause I be up in the mix of action	"	<< endl;
+  cout <<	"	If I'm not at home, puffin lye relaxin'	"	<< endl;
+  cout <<	"	New York got a nigga depressed	"	<< endl;
+  cout <<	"	So I wear a slug-proof underneath my Guess	"	<< endl;
+  cout <<	"	God bless my soul, before I put my foot down and begin to stroll	"	<< endl;
+  cout <<	"	And to the drama I built, and all unfinished beef	"	<< endl;
+  cout <<	"	You will soon be killed, put us together	"	<< endl;
+  cout <<	"	It's like mixin' vodka and milk	"	<< endl;
+  cout <<	"	I'm goin' out blastin', takin' my enemies with me	"	<< endl;
+  cout <<	"	And if not, they scarred, so they will never forget me	"	<< endl;
+  cout <<	"	Lord forgive me the Hennesey got me not knowin' how to act	"	<< endl;
+  cout <<	"	I'm fallin' and I can't turn back	"	<< endl;
+  cout <<	"	Or maybe it's the words from my man Killa Black	"	<< endl;
+  cout <<	"	That I can't say so it's left a untold fact, until my death	"	<< endl;
+  cout <<	"	My goal's to stay alive	"	<< endl;
+  cout <<	"	Survival of the fit only the strong survive	"	<< endl;
+
+
+
+  return 0;
+
+
+}
