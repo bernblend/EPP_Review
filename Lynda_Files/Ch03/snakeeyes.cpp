0af4929a1f2fd4a8fa50638787f8dfa062d17ceb
bernblend <bernblend@gmail.com>
2017-06-23 14:51:56 -0700

    Completed snakeeyes.cpp

--- Lynda_Files/Ch03/snakeeyes.cpp
+++ Lynda_Files/Ch03/snakeeyes.cpp
@@ -1,10 +1,8 @@
 /*
- * snakeeyes.cpp
- *
- *  Created on: May 15, 2014
- *      Author: Peggy Fisher
- */
-
+	snakeeyes.cpp
+	June 23, 2017
+	bernblend
+*/
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -14,17 +12,17 @@ int main()
 {
 	srand(time(0));
 	int count = 0;
-	for (int i = 0;i<100;i++)
+	for (int i = 0;i < 100;i++)
 	{
 		int die1 = rand()%6 + 1;
 		int die2 = rand()%6 + 1;
-		cout<<setw(6)<<die1<<setw(6)<<die2;
+		cout << setw(6) << die1 << setw(6) << die2;
 		if(die1 == 1 && die2 == 1)
 		{
-			cout<<"  Snake Eyes";
+			cout << "  Snake Eyes";
 			count ++;
 		}
-		cout<<endl;
+		cout << endl;
 	}
-	cout<<"There were "<<count<<" snake eyes"<<endl;
+	cout << "There were " << count << " snake eyes." << endl;
 }
