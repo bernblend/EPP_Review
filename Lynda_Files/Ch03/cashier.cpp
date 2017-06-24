a2ae13aaeb21a225e28fc7f6f9038b352081c287
bernblend <bernblend@gmail.com>
2017-06-24 13:08:17 -0700

    Completed cashier.cpp

--- Lynda_Files/Ch03/cashier.cpp
+++ Lynda_Files/Ch03/cashier.cpp
@@ -1,7 +1,7 @@
 /*
-cashier.cpp
-May 21, 2014
-Peggy Fisher
+	cashier.cpp
+	June 24, 2017
+	bernblend
 */
 #include <iostream>
 using namespace std;
@@ -9,41 +9,48 @@ using namespace std;
 void printChange(int&, int&, int&, int&, int&);
 void findCoins(int&, int&, int&, int&, int&);
 
-
 int main()
 {
 	double price;
 	double payment;
-	char answer = 'y';
+	char answer = 'y';   //will enter loop as is.
 	int dollars, quarters, dimes, nickels, pennies;
-	while(answer == 'y')
+	while(answer == 'y')  //y is true, goes into loop.
 	{
-		cout<<"Enter price of an item: "<<endl;
-		cin>>price;
-		cout<<"Enter payment from customer: "<<endl;
-		cin>>payment;
+		cout << "Enter price of an item: " << endl;
+		cin >> price;
+		cout << "Enter payment from customer: " << endl;
+		cin >> payment;
 		double change = payment - price;
 		dollars = change;				//use implicit conversion
-		change = change * 100;			//multiplication
 
-		int coins = change - dollars * 100;
+		change = change * 100;  //convert change to pennies
+
+		cout << "\nTotal change times 100 is: " << change;
+		cout << "\nDollars times 100 is: " << dollars * 100 << "\n\n";
+
+		int coins = change - dollars * 100;//dollars in pennies
+
+		cout << "\nTotal change in cents - Dollars in cents: " << coins << "\n\n";
+
 		findCoins(coins, quarters, dimes, nickels, pennies);
 
 		printChange(dollars, quarters, dimes, nickels, pennies);
-		cout<<"Do you have another transaction?";
-		cin>>answer;
+		cout << "Do you have another transaction?";
+		cin >> answer;
 	}
-	cout<<"Thanks for shopping at Albertsons!"<<endl;
+	cout << "Thanks for shopping at Albertsons!" << endl;
+
 	return 0;
 }
 
 void printChange(int& dol, int& q, int& d, int& n, int& p)
 {
-	cout<<"dollars "<<dol<<endl;
-	cout<<"quarters: "<<q<<endl;
-	cout<<"dimes:  "<<d<<endl;
-	cout<<"nickels: "<<n<<endl;
-	cout<<"pennies: "<<p<<endl;
+	cout << "dollars " << dol << endl;
+	cout << "quarters: " << q << endl;
+	cout << "dimes:  " << d << endl;
+	cout << "nickels: " << n << endl;
+	cout << "pennies: " << p << endl;
 }
 void findCoins(int& coins, int& quarters, int& dimes,
 		int& nickels, int& pennies)
