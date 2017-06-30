f0e867769e453df86108c481dc559da66e82c2e9
bernblend <bernblend@gmail.com>
2017-06-30 02:40:22 -0700

    Added array examples.

---
+++ arrayPlay.cpp
@@ -0,0 +1,50 @@
+/*
+  arrayPlay.cpp
+  June 27, 2017
+  bernblend
+*/
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+double findGreatest(double[], int);
+
+int main(int argc, char const *argv[]) {
+
+  srand(time(0));
+
+  double number[50];
+  double greatestNum;
+
+  for (int i = 0; i < 51; i++) {
+
+    number[i] = rand();
+
+    cout << fixed << i << setw(15) << setprecision(2) << number[i] << i << setw(15) << setprecision(2) << number[i] << i << setw(15) << setprecision(2) << number[i] << endl;
+
+  }
+
+  cout << number[48] << endl;
+  cout << number[49] << endl;
+  cout << number[50] << endl;
+
+  cout << "The greatest number randomly produced was: " << fixed << setw(15) << setprecision(2) << findGreatest(number, 50) << endl;
+
+  return 0;
+}
+
+
+double findGreatest(double n[], int size)
+{
+  double greatest = n[0];   //set to first index
+  for (int i = 1; i < size; i++)
+  {
+    if (n[i] > greatest)
+    {
+      greatest = n[i];
+    }
+  }
+  return greatest;
+}
