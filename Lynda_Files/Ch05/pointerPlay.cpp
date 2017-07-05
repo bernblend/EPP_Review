b1a37fcd5a9cba76622c6e2a3716673013545649
bernblend <bernblend@gmail.com>
2017-07-04 22:46:35 -0700

    Added additional checkpoints on pointerPlay.cpp

--- Lynda_Files/Ch05/pointerPlay.cpp
+++ Lynda_Files/Ch05/pointerPlay.cpp
@@ -12,11 +12,16 @@ using namespace std;
 
 void addFive(int);
 
-
-void addFive(int num)
+/* Make the num Parameter variable as pointer type, to receive the address of numPtr variable from main*/
+void addFive(int *num)
 {
+  /*To get the value, we will deference num by using *(asterisk) operator */
+  cout << "The value of *num in function: " << *num << endl;
+  cout << "The value of &num in function: " << &num << endl;
   cout << "The value of num in function: " << num << endl;
-  num += 5;
+  *num += 5;
+  cout << "The value of *num in function after adding 5: " << *num << endl;
+  cout << "The value of &num in function after adding 5: " << &num << endl;
   cout << "The value of num in function after adding 5: " << num << endl;
 }
 
@@ -25,11 +30,13 @@ int main(int argc, char const *argv[]) {
  int num = 5;
  int* numPtr = &num;
 
- addFive(*numPtr);
+ addFive(numPtr);/*Argument passed will be of pointer type,so that all the changes made by that function will be automatically reflected without even returning any value */
 
  cout << "The value of numPtr in main: " << numPtr << endl;
  cout << "The value of *numPtr in main: " << *numPtr << endl;
+ cout << "The value of &numPtr in main: " << &numPtr << endl;
  cout << "The value of num in main: " << num << endl;
+ cout << "The value of &num in main: " << &num << endl;
 
  return 0;
 }
