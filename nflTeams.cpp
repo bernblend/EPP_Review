e02c534630413e1b94a926112e27f91177ebde13
bernblend <bernblend@gmail.com>
2017-07-04 21:18:45 -0700

    Cleaned up syntax of nflTeams.cpp INCOMPLETE

---
+++ nflTeams.cpp
@@ -0,0 +1,38 @@
+/*
+  nflTeams.cpp
+  June 30, 2017
+  bernblend
+*/
+#include <iostream>
+#include <iomanip>
+#include "NFLteam.h"
+#include "NFLteam.cpp"
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  NFLteam testTeam;
+
+  cout << testTeam << endl;
+
+/*
+  ofstream fout;
+  fout.open("nflTeams.txt");
+*/
+
+
+
+
+/*
+  for (int i = 0; i < 32; i++) {
+    int team = team[i];
+    cout << team << endl;
+  }
+
+*/
+
+  return 0;
+}
