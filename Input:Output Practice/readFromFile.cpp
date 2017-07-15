e2add4974e452d72aa7624f96e80d80ff3a5849e
bernblend <bernblend@gmail.com>
2017-07-15 13:04:53 -0700

    Started readFromFile.cpp

---
+++ Input:Output Practice/readFromFile.cpp
@@ -0,0 +1,27 @@
+/*
+  readFromFile.cpp
+  July 15, 2017
+  bernblend
+*/
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+string getSuperBowl(string [], int);
+
+int main()
+{
+  ifstream inputfile;
+
+  inputfile.open("superBowlList.txt", ios::in);
+
+  char superbowl[100];
+  string superbowls[50];
+  int pos = 0;
+
+  return 0;
+}
