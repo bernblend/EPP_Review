49e0f2ebc1ca417f3eec1af94d4c4dfc6f5980c3
bernblend <bernblend@gmail.com>
2017-07-15 12:01:52 -0700

    Completed challenge.cpp

--- Lynda_Files/Ch05/challenge.cpp
+++ Lynda_Files/Ch05/challenge.cpp
@@ -1,33 +1,35 @@
 /*
-challenge.cpp
-May 23, 2014
-Peggy Fisher
+	challenge.cpp
+	July 13, 2017
+	bernblend
 */
 #include <iostream>
 #include <iomanip>
 using namespace std;
 int main()
 {
+	//Example of Parallel Arrays.
 	string menuItems[] = {"Hot Dogs", "Hamburgers", "Turkey Burgers",
 			"Salad"};
 	double prices[] = {1.50, 3.00, 3.00, 4.00};
 	int choice, quantity;
 	do{
-	cout<<"Choose an item from the menu: "<<endl;
+	cout << "Choose an item from the menu: " << endl;
 
 	for (int i = 0; i < 4; i++)
 	{
-		cout<<i+1<<" ";
-		cout<<setw(20)<<fixed<<setprecision(2)<<left;
-		cout<<menuItems[i]<<prices[i]<<endl;
-	};
-	cout<<"5 Exit\n";
-	cin>>choice;
+		cout << i + 1 << " ";
+		cout << setw(20) << fixed << setprecision(2) << left;
+		cout << menuItems[i] << prices[i] << endl;
+	}
+	cout << "5 Exit\n";
+	cin >> choice;
 	if(choice == 5)
 		break;
-	cout<<"How many? ";
-	cin>>quantity;
-	cout<<"Your total cost is: $"<<quantity * prices[choice-1]<<endl<<endl;
+	cout << "How many? ";
+	cin >> quantity;
+	cout << "Your total cost is: $" << quantity * prices[choice-1] << endl << endl;
 	}	while(choice != 5);
-return 0;
+
+	return 0;
 }
