e74ba2064b9f78628af33aceb98f21e541ab43b0
bernblend <bernblend@gmail.com>
2017-07-19 14:41:57 -0700

    Completed hello-cout.cpp

--- CPP_Essential_Training/ExerciseFiles/Chap01/hello-cout.cpp
+++ CPP_Essential_Training/ExerciseFiles/Chap01/hello-cout.cpp
@@ -1,9 +1,13 @@
-// hello-cout.cpp by Bill Weinman <http://bw.org/>
+/*
+  hello-cout.cpp
+  July 19, 2017
+  bernblend
+*/
 #include <iostream>
 using namespace std;
 
 int main( int argc, char ** argv )
 {
-    cout << "Hello, World!" << endl;
+    cout << "Hello bern!" << endl;
     return 0;
 }
