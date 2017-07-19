36bf52722e417971ffdf55e653c5de37bc89941a
bernblend <bernblend@gmail.com>
2017-07-19 14:43:18 -0700

    Completed working.cpp

--- CPP_Essential_Training/ExerciseFiles/Chap01/working.cpp
+++ CPP_Essential_Training/ExerciseFiles/Chap01/working.cpp
@@ -1,9 +1,19 @@
-// working.cpp by Bill Weinman <http://bw.org/>
-#include <cstdio>
+/*
+  working.cpp
+  July 19, 2017
+  bernblend
+*/
+#include <cstdio>   //allows use of puts().
 using namespace std;
 
 int main( int argc, char ** argv )
 {
-    puts("Hello, World!");
-    return 0;
+  int i = 7;
+  i += 5;
+  printf("The value is %d\n", i);
+
+  int x;
+  printf("x is %d\n", x = 42 * (12 + 14));
+  puts("Hello bern, this is working.cpp.");
+  return 0;
 }
