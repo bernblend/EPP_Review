e43eaa895aaef9b1fd0346d63808c1d2065e2370
bernblend <bernblend@gmail.com>
2017-07-19 14:41:00 -0700

    Completed hello.cpp

--- CPP_Essential_Training/ExerciseFiles/Chap01/hello.cpp
+++ CPP_Essential_Training/ExerciseFiles/Chap01/hello.cpp
@@ -1,9 +1,15 @@
-// hello.cpp by Bill Weinman <http://bw.org/>
-#include <cstdio>
+/*
+  hello.cpp
+  July 19, 2017
+  bernblend
+*/
+#include <iostream>   //allows use of cout
 using namespace std;
 
 int main( int argc, char ** argv )
 {
-    puts("Hello, World!");
+    puts("Hello bern!");   //puts() puts a string in the console and a new line after it.
+    printf("Hello bern!"); //no new line.
+    //Use these instead of cout function.
     return 0;
 }
